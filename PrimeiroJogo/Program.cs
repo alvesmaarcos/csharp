0ecabdadbff621d06df492cbb40c42ca9e067e42
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-28 16:30:08 -0300

    add exercicio primeiro jogo

---
+++ PrimeiroJogo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+Console.Write("Olá! Escolha um número:");
+Random rnd = new Random();
+int randInt = rnd.Next(1, 101);
+
+while (true)
+{
+    int num = int.Parse(Console.ReadLine()!);
+    if(num == randInt)
+    {
+        Console.WriteLine("Parabéns, você acertou!!");
+        break;
+    }
+    else if(randInt > num)
+    {
+        Console.WriteLine("Maior");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Menor");
+        continue;
+    }
+
+}

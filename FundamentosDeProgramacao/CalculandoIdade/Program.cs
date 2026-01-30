f7380b2a353277e8638cb24528b50a87675fc631
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-30 16:07:13 -0300

    add CalcularIdade

---
+++ FundamentosDeProgramacao/CalculandoIdade/Program.cs
@@ -0,0 +1,19 @@
+﻿void CalcularIdade()
+{
+    Console.Write("Olá! Insira o ano de nascimento: ");
+    int anoNasc = int.Parse(Console.ReadLine()!);
+    int anoAtual = 2026;
+
+    int idade = anoAtual - anoNasc;
+
+    if (idade > 0)
+    {
+        Console.WriteLine($"Estamos em {anoAtual}. Como você nasceu em {anoNasc}, possui {idade} anos.");
+    } else
+    {
+        Console.WriteLine("Ano inválido. Tente novamente.");
+        CalcularIdade();
+    }
+}
+
+CalcularIdade();
---
+++ FundamentosDeProgramacao/CalculandoIdade/Readme.md
@@ -0,0 +1,3 @@
+Imagine que você está desenvolvendo um sistema para um cadastro de usuários. Uma das funcionalidades desse sistema é calcular a idade do usuário para oferecer conteúdo e serviços adequados à sua faixa etária.
+
+Escreva um programa em C# que, a partir do ano de nascimento de uma pessoa, calcule a idade com base no ano atual.

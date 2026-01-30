6c43cb52569e9a3568e2713e979f2c5932fba843
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-30 17:45:57 -0300

    add AumentoDeSalario

---
+++ FundamentosDeProgramacao/AumentoDeSalario/Program.cs
@@ -0,0 +1,6 @@
+﻿double salarioAtual = double.Parse(Console.ReadLine()!);
+double percentualAumento = double.Parse(Console.ReadLine()!);
+
+double novoSalario = salarioAtual + (salarioAtual * percentualAumento/100);
+
+Console.WriteLine($"O novo salário é de R${novoSalario:F2}");

5ab2865ca0d0e3e1e9aec344f76e12751d7c26b4
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-30 17:28:51 -0300

    add ControleDeVidas

---
+++ FundamentosDeProgramacao/ControleDeVidas/Program.cs
@@ -0,0 +1,31 @@
+﻿int vidas = 5;
+Erro();
+Acerto();
+Fim();
+
+
+
+void Erro(){
+    setVidas(getVidas()-1);
+    Console.WriteLine($"O jogador errou! Agora a quantidade de vidas é: {getVidas()}");
+}
+
+void Acerto()
+{
+    setVidas(getVidas()+1);
+    Console.WriteLine($"O jogador acertou! Agora a quantidade de vidas é {getVidas()}");
+}
+
+void Fim()
+{
+    Console.WriteLine($"Fim de jogo. Vidas finais: {getVidas()}");
+}
+void setVidas(int qtd)
+{
+    vidas = qtd;
+}
+int getVidas()
+{
+    return vidas;
+}
+

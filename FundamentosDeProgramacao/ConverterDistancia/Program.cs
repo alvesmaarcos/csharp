9b115cd22535dc6fe63ea55d27a40ff5c795b3f8
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-30 17:01:40 -0300

    add ConverterDistancias

---
+++ FundamentosDeProgramacao/ConverterDistancia/Program.cs
@@ -0,0 +1,8 @@
+﻿double ConverterDistancia(double milhas)
+{
+    double km = milhas * 1.60934;
+    return km;
+}
+
+double milhas = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"{milhas} equivale(m) a {ConverterDistancia(milhas)} quilômetro(s).");

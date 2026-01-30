a8fc086e0f35cdaa79d9036a40e289c30b2a1bd0
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-30 17:08:32 -0300

    add Conversão de minutos em horas e minutos

---
+++ FundamentosDeProgramacao/ConverterHorasMinutos/Program.cs
@@ -0,0 +1,19 @@
+﻿void ConverterTempo(int minutos)
+{
+    if(minutos < 60)
+    {
+        Console.WriteLine($"{minutos} minutos");
+    }
+    else if (minutos % 60 != 0)
+    {
+        Console.WriteLine($"{minutos/60} hora(s) e {minutos%60} minuto(s)");
+    }
+    else
+    {
+        Console.WriteLine($"{minutos/60} hora(s)");
+    }
+}
+
+
+int totalMinutos = int.Parse(Console.ReadLine()!);
+ConverterTempo(totalMinutos);
---
+++ FundamentosDeProgramacao/ConverterHorasMinutos/Readme.md
@@ -0,0 +1,5 @@
+Crie um programa que:
+
+Declare uma variável com um valor inteiro representando o total de minutos.
+Calcule quantas horas completas existem nesse valor e quantos minutos restam.
+Exiba o resultado no formato X horas e Y minutos.

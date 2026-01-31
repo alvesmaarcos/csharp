f163cd6ad3953ea213cfc06464dfeb5545235f3a
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-31 17:44:29 -0300

    add SomaDasVendas

---
+++ Praticando-lacos-de-repeticao/SomaDasVendas/Program.cs
@@ -0,0 +1,17 @@
+﻿int sum = 0;
+int val = 0;
+while (true)
+{
+    Console.Write("Digite o valor da venda (ou 0 para sair): ");
+    val = int.Parse(Console.ReadLine()!);
+    if(val == 0)
+    {
+        break;
+    }
+    else
+    {
+        sum += val;
+    }
+}
+
+Console.WriteLine($"Total de vendas do dia: {sum}.");

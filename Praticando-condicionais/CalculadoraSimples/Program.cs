5ed559b69893bc899fdfeb69878f537e00b970e1
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-31 15:19:08 -0300

    add CalculadoraSimples

---
+++ Praticando-condicionais/CalculadoraSimples/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Número 1: ");
+double n1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Operação: ");
+string op = Console.ReadLine()!;
+
+Console.Write("Número 2: ");
+double n2 = double.Parse(Console.ReadLine()!);
+
+
+double res = 0.0;
+bool operacaoValida = true;
+
+
+switch (op)
+{
+    case "+": res = n1 + n2; break;
+    case "-": res = n1 - n2; break;
+    case "/":
+        if(n2 == 0)
+        {
+            Console.WriteLine("Erro: não é possível dividir por 0.");
+            operacaoValida= false;
+        }
+        else
+        {
+            res = n1 / n2;
+        }
+        break;
+    case "*": res = n1 * n2; break;
+    default: 
+        Console.WriteLine("Operação inválida."); 
+        operacaoValida = false;
+    break;
+}
+
+if(operacaoValida)
+{
+    Console.WriteLine($"{res}");
+}
---
+++ Praticando-condicionais/CalculadoraSimples/Readme.md
@@ -0,0 +1,23 @@
+Você precisa construir uma minicalculadora que realiza uma operação matemática com dois números. O usuário deve digitar o primeiro número, o segundo número e qual operação deseja fazer: soma (+), subtração (-), multiplicação (*) ou divisão (/). Com base na operação digitada, o programa deve calcular e exibir o resultado.
+
+Crie um programa que:
+
+- Receba dois números digitados pelo usuário.
+- Pergunte qual a operação matemática deseja realizar (+, -, *, /).
+- Exiba “Operação inválida” caso a entrada seja diferente das operações acima.
+- Exiba o resultado de acordo com a operação escolhida.
+
+### Exemplo de entrada:
+
+Digite o primeiro número:
+5
+
+Digite o segundo número:
+5
+
+Digite a operação (+, -, *, /):
+*
+
+### Saída esperada:
+
+Resultado: 25

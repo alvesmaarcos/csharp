d378d149e4f800a0dd0801c2662d0862623eb883
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-31 18:21:59 -0300

    add GerenciadorDeChamada

---
+++ Praticando-lacos-de-repeticao/GerenciadorDeChamada/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> alunos = new List<string>{"Ana", "Carlos", "Bianca", "João", "Marina" };
+
+while (true)
+{
+    Console.Write("Digite o nome do aluno: ");
+    string aluno = Console.ReadLine()!;
+
+    if (alunos.Contains(aluno))
+    {
+        Console.WriteLine($"Aluno(a) encontrado na posição {alunos.IndexOf(aluno)}"); 
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Aluno(a) não está presente na lista");
+    }
+}

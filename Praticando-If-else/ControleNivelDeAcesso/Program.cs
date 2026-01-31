88cdec84e9ae5c2f2764a459112dbca09816f7b0
alvesmaarcos <alvesmarcos.es@gmail.com>
2026-01-31 14:42:39 -0300

    add ControleNivelDeAcesso

---
+++ Praticando-If-else/ControleNivelDeAcesso/Program.cs
@@ -0,0 +1,16 @@
+﻿int senha = 42;
+int nivelAcesso = 5;
+
+Console.Write("Digite a senha: ");
+int inputSenha = int.Parse(Console.ReadLine()!);
+Console.Write("Digite o nível de acesso: ");
+int inputAcesso = int.Parse(Console.ReadLine()!);
+
+if(inputSenha == senha && inputAcesso >= nivelAcesso)
+{
+    Console.WriteLine("Acesso liberado.");
+}
+else
+{
+    Console.WriteLine("Acesso negado.");
+}
